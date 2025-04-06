3651e227fc1849e6a6c04f2b76d0dc68843639fa
Sifad's Git <sifadsaiful99@gmail.com>
2025-04-06 08:09:24 -0700

    update some code

--- script/script.js
+++ script/script.js
@@ -16,16 +16,3 @@ if (cut) {
     navbar.classList.add('closeSmooth');
   });
 }
-
-// handleClose function to trigger button click when section is clicked
-const handleClose = (click) => {
-  const clickThis = document.getElementById(click); // This is the section with id=hero
-  const cutButton = document.getElementById('close'); // This is the button you want to click
-
-  // Trigger the button's functionality when the section is clicked
-  if (clickThis && cutButton) {
-    clickThis.addEventListener('click', () => {
-      cutButton.click(); // Simulate a click on the "close" button
-    });
-  }
-};
